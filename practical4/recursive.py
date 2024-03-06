5992ccd5a35412b0287f705be919e7ca5f652c26
黄俊然 <rickyh@rickyMacBook-Pro.local>
2024-03-06 13:55:20 +0800

    editrecursive

---
+++ practical4/recursive.py
@@ -0,0 +1,10 @@
+# calculate
+#      a_n=2a_n-1+3
+#      if n<6 keep calculating
+#      if n>=6: Done
+a=4
+for i in range (1,6):
+ print(a)
+ a=2*a+3
+# if i write "a=2*a+3" before "print(a)"
+# i will get the next 5 nunber after 4
