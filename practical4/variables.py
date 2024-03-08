f7fe3d54c0d6cd544beddc2ec1ac928efb780b6b
hykcir <161405538+hykcir@users.noreply.github.com>
2024-03-08 11:25:11 +0800

    Update variables.py

--- practical4/variables.py
+++ practical4/variables.py
@@ -1,3 +1,18 @@
-d>e
-w is True
-
+a=40
+b=36
+c=30
+d=a-c#calculate the difference
+e=b-c#calculate the difference
+if d>e:#compare the two differences and check if d>e
+    print ("a training is better")
+elif d<e:
+    print ("b training is better")
+else:#if they are the same
+    print ("both have the same effect")
+# true a training is better
+X=7>0
+Y=0>7
+print (X ^ Y) #learn this command from a friend
+#if x and y are same (true or false), then print false
+#if x and y are not same, then print true.
+# true
