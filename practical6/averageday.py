c18c050076c9f428d3d23a58519f596c3760d699
黄俊然 <rickyh@rickyMacBook-Pro.local>
2024-03-20 14:08:01 +0800

    edit

--- practical6/averageday.py
+++ practical6/averageday.py
@@ -9,7 +9,7 @@ sizes= list(time.values())
 #to extract datas like labels and sizes from the dictionary
 #now start to plot the pie chart
 plt.figure()
-plt.pie(sizes,labels=labels, startangle=90,autopct="%1.1f%%"#to add percentage into the chart)
+plt.pie(sizes,labels=labels, startangle=90,autopct="%1.1f%%")#to add percentage into the chart
 plt.axis('equal')
 plt.title("Students' time spent of an average day")
 plt.show()
