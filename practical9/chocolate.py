8361f61c321f6dd31538331a47cbe76ed99a57af
黄俊然 <rickyh@rickyMacBook-Pro.local>
2024-04-09 19:48:48 +0800

    editchocolate.py

--- practical9/chocolate.py
+++ practical9/chocolate.py
@@ -1,17 +1,17 @@
 def chocolate_bar(money):
     total=int(money)
     price=7
-    pieces=int(total/price)
+    pieces=int(total/price) #make sure the number is an integer
     left=total-int(pieces)*price
     return pieces,left
 class chocolatebars:
-    def __init__(self, total, left):
-        self.total = total
-        self.left = left
+    def __init__(self, pieces, left):
+        self.pieces = pieces #align the value of "pieces" calculated in advance to the instance variable "pieces"
+        self.left = left# same as the upper line
 
     def print(self):
-        print(f"The amount of chocolate bars you can have: {self.total}, The change left over is: {self.left}")
+        print(f"The amount of chocolate bars you can have: {self.pieces}, The change left over is: {self.left}")
 user_input=input("Enter the money you have:")
-pieces, left=chocolate_bar(user_input)
+pieces, left=chocolate_bar(user_input)# Create an instance of the chocolatebars class with the calculated pieces and left values
 b=chocolatebars(pieces, left)
 b.print()
